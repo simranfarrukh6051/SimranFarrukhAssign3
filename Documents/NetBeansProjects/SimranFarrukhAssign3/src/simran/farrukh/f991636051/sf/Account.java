5ca6dd82ad29da68df11b1180710bed37b11c5d7
simranfarrukh6051 <84728842+simranfarrukh6051@users.noreply.github.com>
2021-07-29 19:55:13 -0400

    Create an abstract Account class with two attributes email and password. Make the class immutable.

---
+++ Documents/NetBeansProjects/SimranFarrukhAssign3/src/simran/farrukh/f991636051/sf/Account.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simran.farrukh.f991636051.sf;
+
+/**
+ *
+ * @author Simran
+ */
+public abstract class Account {
+    private String email;
+    private String password;
+}
